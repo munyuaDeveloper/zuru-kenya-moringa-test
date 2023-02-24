398dddfb41b6c2f0024fc7f12fa89efae2db94ee
munyuaDeveloper <munyuapeter06@gmail.com>
2023-02-24 16:42:45 +0300

    fix controllers

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -14,7 +14,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   // 2) Build template
   // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
-    title: 'All Zuru-kenya-moringa-test',
+    title: 'All Tours',
     tours
   });
 });
